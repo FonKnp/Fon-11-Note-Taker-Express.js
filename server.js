536631bc224aa5a1d570b507eeea1b79ad8571e3
FonKnp <fonnfonk@gmail.com>
2024-04-24 18:56:40 -0400

    add routes folder more modular

--- server.js
+++ server.js
@@ -4,8 +4,14 @@ const path = require('path');
 const fs = require('fs');
 const uuid = require('uuid');
 
+// require html and api routes
+const htmlRoutes = require('./routes/indexRoute');
+const apiRoutes = require('./routes/apiRoutes');
+
 // create express app
 const app = express();
 
 // middleware for parsing json 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
